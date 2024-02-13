13abeca04679241480970c665514b8ed0f7730ab
fredy227 <153804479+fredy227@users.noreply.github.com>
2024-02-13 00:28:42 -0500

    up json

--- calendar/src/index.js
+++ calendar/src/index.js
@@ -5,5 +5,5 @@ const callback = (event) => {
     alert("Usted a precionado el dia: (" +event.target.dataset.date+")");
 }
 
-const calendar = new Calendar(callback,"es","/calendar/src/example/citas.json")
+const calendar = new Calendar(callback,"es","./calendar/src/example/citas.json")
 calendar.render(container)
