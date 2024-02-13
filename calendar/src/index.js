3f129b53cd006ea36468a4f267da21c62623d8dd
fredy227 <153804479+fredy227@users.noreply.github.com>
2024-02-13 00:16:58 -0500

    json

--- calendar/src/index.js
+++ calendar/src/index.js
@@ -1,4 +1,4 @@
 import { Calendar } from "./js/class/calendar.js"
 const container = document.querySelector("#calendar-container")
-const calendar = new Calendar((e)=>{console.log(e)},"es","/calendar/src/example/citas.json")
+const calendar = new Calendar((e)=>{console.log(e)},"es","./calendar/src/example/citas.json")
 calendar.render(container)
